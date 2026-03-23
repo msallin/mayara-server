f2f2795b2a9df3b63f5cdf6cbf4cf3ee40c8e659
Kees Verruijt <kees@verruijt.net>
2026-03-24 00:34:18 +0100

    repair test

--- test/subscribe.sh
+++ test/subscribe.sh
@@ -6,16 +6,15 @@ set -x
 HOST=${1:-10.56.0.1}
 
 BASE_URL="http://${HOST}:6502"
-V3="${BASE_URL}/v3/api"
+V2="${BASE_URL}/signalk/v2/api/vessels/self/radars"
 
-curl -s "${V3}/radars" | jq 
-radars=($(curl -s "${V3}/radars" | jq -r '.radars | keys[]'))
+curl -s "${V2}" | jq 
+radars=($(curl -s "${V2}" | jq -r '.radars | keys[]'))
 echo "Radars: ${radars}"
 
 for radar in ${radars}
 do
-  curl -s "${V3}/radars" | jq -r ".radars.${radar} | keys[]"
-  streamUrl=$(curl -s "${V3}/radars" | jq -r ".radars.${radar}.streamUrl")"?subscribe=none"
+  streamUrl=$(curl -s "${V2}" | jq -r ".radars.${radar}.streamUrl")"?subscribe=all"
   echo "Connecting to $streamUrl"
   websocat "${streamUrl}"
 done
